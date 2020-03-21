36e36aeaf8a8510ac906c4ec00b0e43222ac80a7
Christine Ramos <christineramos12@gmail.com>
2020-03-20 14:23:29 -1000

    first commit

---
+++ src/app/owner-form/owner-form.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Owner } from '../owner';
+
+@Component({
+  selector: 'app-owner-form',
+  templateUrl: './owner-form.component.html',
+  styleUrls: ['./owner-form.component.css']
+})
+export class OwnerFormComponent {
+
+  names = ['John Doe', 'Jane Doe'];
+  model = new Owner('Doe', 'John', 808, 123, 4567, 'email@email.com', 'A1234B0123');
+  submitted = false;
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+}
---
+++ src/app/owner.ts
@@ -0,0 +1,11 @@
+export class Owner {
+  constructor(
+    public lastname: string,
+    public firstname: string,
+    public areaCode: number,
+    public phone3: number,
+    public phone4: number,
+    public email: string,
+    public microchip: string
+  ) {}
+}
